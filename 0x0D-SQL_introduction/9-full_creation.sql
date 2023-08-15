356cdfe41c23c7935f1388b4c60fadfa78f39241
KhileM <sakhile.motha.254@gmail.com>
2023-08-15 12:20:45 -0700

    a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,20 +1,6 @@
-#!/bin/bash
-
-# Get the database name from the command-line argument
-db_name="$1"
-
-# Define the SQL script
-sql_script=$(cat <<EOF
-CREATE TABLE IF NOT EXISTS ${db_name}.second_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256),
-    score INT
-);
-
-INSERT INTO ${db_name}.second_table (id, name, score)
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
-EOF
-)
-
-# Execute the SQL script using MySQL command-line tool
-mysql -u your_username -p your_password -e "${sql_script}"
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
