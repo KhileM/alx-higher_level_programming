e78dfb528edf12ef0b99d3d6e1a4ec7320dbb6d6
KhileM <sakhile.motha.254@gmail.com>
2023-09-29 09:36:48 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
-
-print("Body response:")
-print("    - type:", type(body))
-print("    - content:", body)
-print("    - utf8 content:", utf8_content)
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
